43a08e76ba3ba62a049f77ff4cffee51ff4ad49c
s3afeen <zaidsafeen30@gmail.com>
2024-06-23 00:39:41 +0300

    js-3

---
+++ task3.js
@@ -0,0 +1,70 @@
+//-------------------1
+// function SayHello(name)
+// {
+//     console.log(`hello ${name}`);
+// }
+// SayHello("zaid");
+
+
+
+//-------------------2
+// function square(num)
+// {
+// console.log(num*num);
+// }
+// console.log(square(3));
+
+
+
+//-------------------3
+// let multiply = (num1,num2) => num1 * num2
+
+// console.log(multiply(2 ,3))
+// console.log(multiply(4,5))
+//----------------------
+// let multiply = num * num1;
+// function multiply()
+// {
+// console.log(num*num1);
+// }
+// console.log(3,3);
+
+
+
+//-------------------4
+// let car = {
+//  carName : "nissan",
+//  carYear : 2015,
+//  carModel : "vtc",
+// }
+// console.log(car);
+
+
+
+//-------------------5*******************
+// let colors = ['red' ,'blue' ,'black']
+// for (let x = 0 ; x < 3 ; x++)
+//     {
+//         console.log(colors);
+//         console.log(text);
+
+//     }
+
+
+
+//-------------------6
+// let zaid =  "hello";
+// console.log(zaid.length);
+
+
+//-------------------7
+// function upper(str) 
+//     {
+//         return str.toUpperCase();
+//     }
+
+//     console.log(upper("hello"));
+
+
+
+//-------------------8
